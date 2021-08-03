29da1ab52c88ec0e1545b9481aa36063ca66fba4
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-03 15:35:56 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 8;
+        if(num1>num2){
+            System.out.println("Первое число больше второго");
+        }
+        else if(num1<num2){
+            System.out.println("Первое число меньше второго");
+        }
+        else{
+            System.out.println("Числа равны");
+        }
+    }
+}
